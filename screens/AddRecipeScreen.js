2ce623d0c0f3b66c8d9f10b2cc5cefe1d5f140a4
Joe <joefuller042@gmail.com>
2023-06-09 14:35:28 +0100

    update db url

--- screens/AddRecipeScreen.js
+++ screens/AddRecipeScreen.js
@@ -5,9 +5,26 @@ import axios from "axios";
 const AddRecipeScreen = () => {
   const [url, setUrl] = useState("");
 
-  const handleAddRecipe = () => {
-    // Implement the logic to fetch recipe information and add it to the database
-    // using the provided URL
+  const handleAddRecipe = async () => {
+    try {
+      const response = await axios.post(
+        "http://recipe-app.cyclic.app/recipes",
+        {
+          url,
+        }
+      );
+      if (response.status === 201) {
+        // Recipe added successfully
+        // You can provide feedback to the user, such as displaying a success message
+        console.log("Recipe added successfully!");
+        // Reset the input field
+        setUrl("");
+      }
+    } catch (error) {
+      // Error occurred while adding the recipe
+      // You can handle the error and provide appropriate feedback to the user
+      console.error("Failed to add recipe:", error);
+    }
   };
 
   return (
