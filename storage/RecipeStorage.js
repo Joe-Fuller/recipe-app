d862795e1987d23dba289b5570f37d19e706e564
Joe <joefuller042@gmail.com>
2023-06-19 15:21:58 +0100

    add recipe storage

---
+++ storage/RecipeStorage.js
@@ -0,0 +1,29 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const saveRecipe = async (key, data) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(data));
+    console.log(`Data saved successfully with key: ${key}`);
+  } catch (error) {
+    console.error("Error saving data:", error);
+  }
+};
+
+export const getRecipe = async (key) => {
+  try {
+    const dataString = await AsyncStorage.getItem(key);
+    if (dataString !== null) {
+      const data = JSON.parse(dataString);
+      console.log(`Data retrieved successfully with key: ${key}`);
+      return data;
+    }
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+  }
+  return null;
+};
+
+export default {
+  saveRecipe,
+  getRecipe,
+};
