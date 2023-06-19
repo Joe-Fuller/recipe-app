a0e594d3c77661d0cb11f53f4033839d73916894
Joe <joefuller042@gmail.com>
2023-06-19 16:15:11 +0100

    remove constant key from ShoppingListStorage functions

--- storage/ShoppingListStorage.js
+++ storage/ShoppingListStorage.js
@@ -1,17 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const saveShoppingList = async (key, data) => {
+export const saveShoppingList = async (data) => {
   try {
-    await AsyncStorage.setItem(key, JSON.stringify(data));
+    await AsyncStorage.setItem("shoppingList", JSON.stringify(data));
     console.log(`Data saved successfully with key: ${key}`);
   } catch (error) {
     console.error("Error saving data:", error);
   }
 };
 
-export const getShoppingList = async (key) => {
+export const getShoppingList = async () => {
   try {
-    const dataString = await AsyncStorage.getItem(key);
+    const dataString = await AsyncStorage.getItem("shoppingList");
     if (dataString !== null) {
       const data = JSON.parse(dataString);
       console.log(`Data retrieved successfully with key: ${key}`);
