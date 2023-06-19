9548257048a2f17ad84a4f28b35b8cd56d590ac7
Joe <joefuller042@gmail.com>
2023-06-19 15:32:37 +0100

    update sortIngredients to default ingredient name to an empty string if one cannot be found

--- utils/sortIngredients.js
+++ utils/sortIngredients.js
@@ -1,9 +1,9 @@
 const sortIngredients = (ingredients) => {
   return ingredients.sort((a, b) => {
-    const nameA = a.name || a.ingredient_name;
-    const nameB = b.name || b.ingredient_name;
+    const nameA = a.name || a.ingredient_name || "";
+    const nameB = b.name || b.ingredient_name || "";
     return nameA.localeCompare(nameB);
   });
 };
 
-module.exports = { sortIngredients };
+module.exports = sortIngredients;
