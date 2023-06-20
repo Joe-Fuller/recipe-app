8caf6168a4571113ebf9559532ff7ae7637c118d
Joe <joefuller042@gmail.com>
2023-06-20 14:46:00 +0100

    create RecipeCard.js

---
+++ components/RecipeCard.js
@@ -0,0 +1,26 @@
+import { TouchableOpacity } from "react-native";
+import recipeCardStyles from "../styles/recipeCardStyles";
+import { useNavigation } from "@react-navigation/core";
+
+const RecipeCard = (recipe) => {
+  const navigation = useNavigation();
+
+  const navigateToRecipe = () => {
+    navigation.navigate("SingleRecipe", recipe.name);
+  };
+
+  return (
+    <TouchableOpacity
+      style={recipeCardStyles.recipeContainer}
+      onPress={() => navigateToRecipe()}
+    >
+      <Image
+        source={{ uri: recipe.imageLink }}
+        style={recipeCardStyles.recipeImage}
+      ></Image>
+      <Text style={recipeCardStyles.recipeTitle}>{recipe.name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default RecipeCard;
