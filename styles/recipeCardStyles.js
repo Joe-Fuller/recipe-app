fefc4fe3224b1694a7aff23bbe2fef581040ea1d
Joe <joefuller042@gmail.com>
2023-06-22 15:51:22 +0100

    update recipeCardStyles to use settings

--- styles/recipeCardStyles.js
+++ styles/recipeCardStyles.js
@@ -1,23 +1,25 @@
 import { StyleSheet } from "react-native";
 
-const recipeCardStyles = StyleSheet.create({
-  recipeContainer: {
-    flex: 1,
-    margin: 8,
-    alignItems: "center",
-  },
-  recipeImage: {
-    width: "100%",
-    height: "auto",
-    aspectRatio: 1,
-    borderRadius: 8,
-    marginBottom: 8,
-  },
-  recipeTitle: {
-    fontSize: 16,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+const recipeCardStyles = (settings) => {
+  return StyleSheet.create({
+    recipeContainer: {
+      flex: 1,
+      margin: 8,
+      alignItems: "center",
+    },
+    recipeImage: {
+      width: "100%",
+      height: "auto",
+      aspectRatio: 1,
+      borderRadius: 8,
+      marginBottom: 8,
+    },
+    recipeTitle: {
+      fontSize: 16,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+  });
+};
 
 export default recipeCardStyles;
