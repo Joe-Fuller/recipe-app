dc69bc5c83e50c503dd2890ea09d9cfc5feb9c5f
Joe <joefuller042@gmail.com>
2023-06-22 15:50:07 +0100

    update commonStyles to be dynamic and use settings

--- styles/commonStyles.js
+++ styles/commonStyles.js
@@ -1,41 +1,43 @@
 import { StyleSheet, StatusBar } from "react-native";
 
-const commonStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 16,
-    marginTop: StatusBar.currentHeight,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 32,
-  },
-  button: {
-    backgroundColor: "#f4511e",
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 16,
-    width: "100%",
-  },
-  removeButton: {
-    backgroundColor: "#f4511e",
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-    marginBottom: 16,
-    marginLeft: "auto",
-    width: "auto",
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+const getDynamicStyles = (settings) => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 16,
+      marginTop: StatusBar.currentHeight,
+    },
+    title: {
+      fontSize: settings.textSize + 10,
+      fontWeight: "bold",
+      marginBottom: 32,
+    },
+    button: {
+      backgroundColor: "#f4511e",
+      padding: 10,
+      borderRadius: 5,
+      marginBottom: 16,
+      width: "100%",
+    },
+    removeButton: {
+      backgroundColor: "#f4511e",
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+      borderRadius: 5,
+      marginBottom: 16,
+      marginLeft: "auto",
+      width: "auto",
+      alignItems: "center",
+    },
+    buttonText: {
+      color: "white",
+      fontSize: settings.textSize,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+  });
+};
 
-export default commonStyles;
+export default getDynamicStyles;
