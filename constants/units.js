6c175a89bdd4da2332025598330a94c23b769a13
Joe <joefuller042@gmail.com>
2023-06-26 16:12:00 +0100

    add many more unit constants

--- constants/units.js
+++ constants/units.js
@@ -1,5 +1,35 @@
 export const unitCorrelation = {
+  // Volume units
   tsp: ["tsp", "tsps", "teaspoon", "teaspoons"],
   tbsp: ["tbsp", "tbsps", "tablespoon", "tablespoons"],
   cup: ["cup", "cups"],
+  oz: ["oz", "ounce", "ounces"],
+  fl_oz: ["fl oz", "fluid ounce", "fluid ounces"],
+  pt: ["pt", "pint", "pints"],
+  qt: ["qt", "quart", "quarts"],
+  gal: ["gal", "gallon", "gallons"],
+  ml: ["ml", "milliliter", "milliliters"],
+  l: ["l", "liter", "liters"],
+
+  // Weight units
+  mg: ["mg", "milligram", "milligrams"],
+  g: ["g", "gram", "grams"],
+  kg: ["kg", "kilogram", "kilograms"],
+  lb: ["lb", "pound", "pounds"],
+  oz_weight: ["oz", "ounce", "ounces"],
+
+  // Count units
+  piece: ["piece", "pieces"],
+  slice: ["slice", "slices"],
+  whole: ["whole"],
+  package: ["package", "packages"],
+  can: ["can", "cans"],
+  jar: ["jar", "jars"],
+
+  // Length units
+  inch: ["inch", "inches"],
+  cm: ["cm", "centimeter", "centimeters"],
+  m: ["m", "meter", "meters"],
+  ft: ["ft", "foot", "feet"],
+  yard: ["yard", "yards"],
 };
