da685fb3537e0c55491311cc7802cde0f3fa46ca
Joe <joefuller042@gmail.com>
2023-06-26 16:31:47 +0100

    fix splitIngredient string with fractions

--- utils/splitIngredientString.js
+++ utils/splitIngredientString.js
@@ -14,49 +14,30 @@ function splitIngredientString(ingredientString) {
     return { name, amount, units };
   }
 
-  // // each character before the first letter is the amount
-  // // the next word is the units
-  // // everything remaining is the name
-
-  // const re = /[a-z ,.]/i;
-
-  // // the amount is the bit before the regex match
-  // let pos = ingredientString.search(re);
-  // amount = ingredientString.slice(0, pos).trim();
-
-  // // split the string with the amount removed into words
-  // let remainingWords = ingredientString.slice(pos).trim().split(" ");
-
-  // // the first word is the units
-  // units = remainingWords[0].trim();
-
-  // // the remainder is the name
-  // name = remainingWords.slice(1).join(" ").trim();
-
-  // return { name, amount, units };
-
   // Check for units
   for (const [unitKey, unitValues] of Object.entries(unitCorrelation)) {
-    const foundUnit = unitValues.find((unit) => ingredient.includes(unit));
+    const foundUnit = unitValues.find((unit) =>
+      ingredientString.includes(unit)
+    );
     if (foundUnit) {
       units = unitKey;
-      ingredient = ingredient.replace(new RegExp(foundUnit, "gi"), "").trim();
+      ingredientString = ingredientString
+        .replace(new RegExp(foundUnit, "gi"), "")
+        .trim();
       break;
     }
   }
 
   // Split remaining string into amount and ingredient
-  const parts = ingredient.split(/\s+/);
-  if (parts.length > 0) {
-    const firstPart = parts[0];
-    if (!isNaN(firstPart)) {
-      amount = firstPart;
-      parts.shift();
-    }
-    ingredientName = parts.join(" ");
+  const parts = ingredientString.split(/\s+/);
+  const amountMatch = parts[0].match(/^(\d+(\.\d+)?|\d+\/\d+)$/);
+  if (amountMatch) {
+    amount = amountMatch[0];
+    parts.shift();
   }
+  name = parts.join(" ");
 
-  return { amount, unit, ingredient: ingredientName };
+  return { amount, units, name };
 }
 
 module.exports = splitIngredientString;
