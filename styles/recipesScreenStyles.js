ba7ccdd0941786cfe5e93048be38b2c6c995e82b
Joe <joefuller042@gmail.com>
2023-06-27 12:04:48 +0100

    add input styling to RecipesScreen

--- styles/recipesScreenStyles.js
+++ styles/recipesScreenStyles.js
@@ -8,6 +8,20 @@ const recipesScreenStyles = (settings) => {
       marginTop: StatusBar.currentHeight,
       backgroundColor: settings.theme === "dark" ? "black" : "white",
     },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 16,
+    },
+    input: {
+      fontSize: settings.textSize,
+      borderWidth: 1,
+      borderColor: settings.theme === "dark" ? "#fff" : "#000",
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 0,
+      color: settings.theme === "dark" ? "white" : "black",
+    },
   });
 };
 
